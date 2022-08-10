4cb7d2ba6462c86d0389ca4f81d74e968a81e7d1
codechappie <davidproyecto01@gmail.com>
2022-08-09 19:07:51 -0500

    updated

--- pages/trabajadores.js
+++ pages/trabajadores.js
@@ -5,17 +5,17 @@ const trabajadores = () => {
     return (
         <div className='trabajadores'>
             <h1>Trabajadores</h1>
-            <ul>
+            <div className='grid-t'>
                 {
                     data.map(trabajador => (
-                        <li  className='card' key={trabajador.username}>
+                        <a href={`/trabajador/${trabajador.username}`} className='card' key={trabajador.username}>
                             <small>{trabajador.username}</small>
                             <h2>{trabajador.name} {trabajador.lastname}</h2>
                             <img src={trabajador.profile_image} alt={trabajador.name} />
-                        </li>
+                        </a>
                     ))
                 }
-            </ul>
+            </div>
         </div>
     )
 }
